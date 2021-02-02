b7be7920fdc6d9936be9d438fd5f058c4ee0fb56
Saloni Gupta <saloniguptasg12@gmail.com>
2021-02-02 10:18:00 +0530

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/106245736

---
+++ codeforces/1455/C.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+int main()
+{
+    //OJ;
+    int t;
+    cin >> t;
+    while(t--){
+        int A, B;
+        cin >> A >> B;
+        cout << A-1 << " " << B << endl;
+    }
+    return 0;
+}
