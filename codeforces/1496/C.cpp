bde1411313bc4af72126d7263e7f4cf2a1eb66b2
Saloni Gupta <saloniguptasg12@gmail.com>
2021-03-10 20:40:00 +0530

    Add solution for problem `C - Diamond Miner` Link: https://codeforces.com/contest/1496/problem/C Tags: geometry, greedy, sortings, *1200 Ref: https://codeforces.com/contest/1496/submission/109632509

--- codeforces/1496/C.cpp
+++ codeforces/1496/C.cpp
@@ -4,7 +4,7 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
- 
+
 #define OJ                            \
     freopen("input.txt", "r", stdin); \
     freopen("output.txt", "w", stdout);
@@ -12,40 +12,42 @@ using namespace std;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL);
- 
 
-vector<double> xaxis, yaxis;
- 
+
+vector<double>vx,vy;
+
 int main()
 {
    // OJ;
     FIO;
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
+ 
+    int line,n,i,j;
+    cin>>line;
+    while(line--)
+    {
         cin >> n;
-        int a, b;
         for(int i=0; i<2*n; i++){
+            int a, b;
             cin >> a >> b;
             if(a==0){
-                yaxis.push_back(abs(b));
+                vy.push_back(abs(b));
             } else {
-                xaxis.push_back(abs(a));
+                vx.push_back(abs(a));
             }
         }
- 
-        sort(yaxis.begin(), yaxis.end());
-        sort(xaxis.begin(), xaxis.end());
-        double fin = 0.00;
-        for(int i=0; i<n; i++){
-            fin += sqrt(yaxis[i] * yaxis[i] + xaxis[i] * xaxis[i]); 
+        sort(vx.begin(),vx.end());
+        sort(vy.begin(),vy.end());
+        double ans=0.00;
+        long double fin = 0.00;
+        for(i=0; i<n; i++){
+            fin += (double)sqrt((double)(vy[i] * vy[i]) + (double)(vx[i] * vx[i])); 
         }
  
-        
-        std::cout << std::fixed << std::setprecision(15) << fin << endl;
-        xaxis.clear();
-        yaxis.clear();
+        cout<<setprecision(15);
+        cout<<fixed;
+        cout<<fin<<endl;
+ 
+        vx.clear();
+        vy.clear();
     }
-    return 0;
 }
